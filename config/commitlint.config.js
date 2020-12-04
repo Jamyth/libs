8ed3386db89813f4704961b7b9d08c9eba0c9d14
Jamyth Luk <jamythluk@Jamyths-MacBook-Pro.local>
2020-12-04 16:53:41 +0800

    [CHORE]: root: fix husky and generate commitlint.config.js

---
+++ config/commitlint.config.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = exports.RuleSeverity = void 0;
+exports.RuleSeverity = {
+    Disabled: 0,
+    Warning: 1,
+    Error: 2,
+};
+exports.config = {
+    extends: ["@commitlint/config-conventional"],
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /\[(.*)\]: ([\w-]+(?=: ))?(.+)$/,
+            headerCorrespondence: ["type", "scope", "subject"],
+        },
+    },
+    rules: {
+        "type-case": [exports.RuleSeverity.Error, "always", "upper-case"],
+        "type-enum": [
+            exports.RuleSeverity.Error,
+            "always",
+            [
+                "CHORE",
+                "FEATURE",
+                "FIX",
+                "FORMAT",
+                "REFACTOR",
+            ],
+        ],
+        "scope-case": [exports.RuleSeverity.Error, "always", "lower-case"],
+        "subject-case": [exports.RuleSeverity.Error, "always", "sentence-case"],
+        "header-max-length": [exports.RuleSeverity.Warning, "always", 100],
+        "body-max-length": [exports.RuleSeverity.Warning, "always", 100],
+        "footer-max-line-length": [exports.RuleSeverity.Warning, "always", 100],
+    },
+};
