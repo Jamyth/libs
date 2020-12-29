b135be10d1668042a59b63a5c4071366d32e1bde
Jamyth Luk <jamythluk@Jamyths-MacBook-Pro.local>
2020-12-29 17:11:04 +0800

    [FEATURE]: web-util: added new Type

--- packages/web-util/dist/type.d.ts
+++ packages/web-util/dist/type.d.ts
@@ -2,3 +2,8 @@ export interface ControlledFormValue<T> {
     value: T;
     onChange: (value: T) => void;
 }
+export declare type MarkAsOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+export declare type MarkAsRequired<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
+export declare type MarkAsNullable<T, K extends keyof T> = Omit<T, K> & {
+    [P in K]: T[P] | null;
+};
--- packages/web-util/src/type.ts
+++ packages/web-util/src/type.ts
@@ -2,3 +2,10 @@ export interface ControlledFormValue<T> {
   value: T;
   onChange: (value: T) => void;
 }
+
+export type MarkAsOptional<T, K extends keyof T> = Omit<T, K> &
+  Partial<Pick<T, K>>;
+export type MarkAsRequired<T, K extends keyof T> = Omit<T, K> &
+  Required<Pick<T, K>>;
+export type MarkAsNullable<T, K extends keyof T> = Omit<T, K> &
+  { [P in K]: T[P] | null };
